2c9ce9bc8d3e0a1929d6bd7d36694629b6a4d0f8
Shreesha04 <shreesha467@gmail.com>
2023-12-25 18:33:11 +0530

    Initial commit

---
+++ dashboard.py
@@ -0,0 +1,51 @@
+from tkinter import*
+from PIL import Image,ImageTk
+class IMS:
+    def __init__(self,root):
+        self.root=root
+        self.root.geometry("1350x700+0+0")
+        self.root.title("Inventory Management System")
+        self.root.config(bg="white")
+        
+        title=Label(self.root,text="Inventory Management System",font=("times new roman",40,"bold"),bg="#0000EE",fg="#FFEBCD",anchor="w",padx=20).place(x=0,y=0,relwidth=1,height=70)
+        btn_logout=Button(self.root,text="Logout",font=("times new roman",15,"bold"),bg="yellow",cursor="hand2").place(x=1150,y=10,height=50,width=150)
+        self.lbl_clock=Label(self.root,text="Welcome To Inventory Management System\t\t Date:DD-MM-YYYY\t\t Time: HH-MM-SS",font=("times new roman",15,),bg="#E0EEEE",fg="#000000")
+        self.lbl_clock.place(x=0,y=70,relwidth=1,height=30)
+        self.MenuLogo=Image.open("images/menu_im.png")
+        self.MenuLogo=self.MenuLogo.resize((200,200),Image.LANCZOS)
+        self.MenuLogo=ImageTk.PhotoImage(self.MenuLogo)
+        LeftMenu=Frame(self.root,bd=2,relief=RIDGE,bg="white")
+        LeftMenu.place(x=0,y=102,width=200,height=565)
+        
+        lbl_menuLogo=Label(LeftMenu,image=self.MenuLogo)
+        lbl_menuLogo.pack(side=TOP,fill=X)
+        
+        lbl_menu=Label(LeftMenu,text="Menu",font=("times new roman",20),bg="#458B00").pack(side=TOP,fill=X)
+        btn_employee=Button(LeftMenu,text="Employee",font=("times new roman",20,"bold"),bg="white",bd=3,cursor="hand2").pack(side=TOP,fill=X)
+        btn_supplier=Button(LeftMenu,text="Supplier",font=("times new roman",20,"bold"),bg="white",bd=3,cursor="hand2").pack(side=TOP,fill=X)
+        btn_category=Button(LeftMenu,text="Category",font=("times new roman",20,"bold"),bg="white",bd=3,cursor="hand2").pack(side=TOP,fill=X)
+        btn_products=Button(LeftMenu,text="Products",font=("times new roman",20,"bold"),bg="white",bd=3,cursor="hand2").pack(side=TOP,fill=X)
+        
+        btn_sales=Button(LeftMenu,text="Sales",font=("times new roman",20,"bold"),bg="white",bd=3,cursor="hand2").pack(side=TOP,fill=X)
+        btn_exit=Button(LeftMenu,text="Exit",font=("times new roman",20,"bold"),bg="white",bd=3,cursor="hand2").pack(side=TOP,fill=X)
+        
+        self.lbl_employee=Label(self.root,text="Total Employees\n[0]",bg="#6495ED",fg="white",font=("times new roman",20,"bold"))
+        self.lbl_employee.place(x=300,y=120,height=150,width=300)
+        
+        self.lbl_supplier=Label(self.root,text="Total Suppliers\n[0]",bg="#00EEEE",fg="white",font=("times new roman",20,"bold"))
+        self.lbl_supplier.place(x=650,y=120,height=150,width=300)
+        
+        self.lbl_category=Label(self.root,text="Total Categories\n[0]",bg="#FFB90F",fg="white",font=("times new roman",20,"bold"))
+        self.lbl_category.place(x=1000,y=120,height=150,width=300)
+        
+        self.lbl_product=Label(self.root,text="Total Products\n[0]",bg="#FF7F00",fg="white",font=("times new roman",20,"bold"))
+        self.lbl_product.place(x=300,y=300,height=150,width=300)
+        
+        self.lbl_sales=Label(self.root,text="Total Sales\n[0]",bg="#BF3EFF",fg="white",font=("times new roman",20,"bold"))
+        self.lbl_sales.place(x=650 ,y=300,height=150,width=300)
+        
+        
+        
+root=Tk()
+obj=IMS(root)
+root.mainloop()
