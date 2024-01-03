93f67308a37f70b6cbd7ad543016deb56ec089b0
Shreesha04 <shreesha467@gmail.com>
2024-01-03 23:55:48 +0530

    2nd vdo
    completed till 40 min
    employee details- done
    added the buttons

--- employee.py
+++ employee.py
@@ -10,14 +10,96 @@ class employeeClass:
         self.root.config(bg="white")
         self.root.focus_force()
 
+        self.var_searchby=StringVar()
+        self.var_searchtxt=StringVar()
+
+        self.var_emp_id=StringVar()
+        self.var_gender=StringVar()
+        self.var_contact=StringVar()
+        self.var_name=StringVar()
+        self.var_dob=StringVar()
+        self.var_doj=StringVar()
+        self.var_email=StringVar()
+        self.var_pass=StringVar()
+        self.var_utype=StringVar()
+        self.var_salary=StringVar()
+
+
+
         #searchbar
 
         SearchFrame=LabelFrame(self.root,text="Search Employee",font=("goudy old style",12,"bold"),bd=4,relief=RIDGE,bg="white")
         SearchFrame.place(x=250,y=20,width=600,height=70)
 
-        cmb_search=ttk.Combobox(SearchFrame,values=("Select","Email ID","Name","Contact"),state='readonly',justify=CENTER,font=("goudy old style",15))
+        cmb_search=ttk.Combobox(SearchFrame,textvariable=self.var_searchby,values=("Select","Name","Email ID","Contact"),state='readonly',justify=CENTER,font=("goudy old style",15))
         cmb_search.place(x=10,y=10,width=180)
         cmb_search.current(0)
+
+        txt_search=Entry(SearchFrame,textvariable=self.var_searchtxt,font=('goudy old style',15),bg="lightyellow").place(x=200,y=10)
+        btn_search=Button(SearchFrame,text="Search",font=("goudy old style",15),bg="#4caf50",fg="white",cursor="hand2").place(x=410,y=9,width=150,height=30)
+
+        title=Label(self.root,text="Employee Details",font=("goudly old style",15),bg="#0f4d7d",fg="white").place(x=50,y=100,width=1000)
+
+        #row1
+        
+        lbl_empid=Label(self.root,text="Emp ID",font=("goudly old style",15),bg="white").place(x=50,y=150)
+        lbl_gender=Label(self.root,text="Gender",font=("goudly old style",15),bg="white").place(x=350,y=150)
+        lbl_contact=Label(self.root,text="Contact",font=("goudly old style",15),bg="white").place(x=750,y=150)
+
+        txt_empid=Entry(self.root,textvariable=self.var_emp_id,font=("goudly old style",15),bg="light yellow").place(x=150,y=150,width=180)
+        cmb_gender=ttk.Combobox(self.root,textvariable=self.var_gender,values=("Select","Male","Female","Other"),state='readonly',justify=CENTER,font=("goudy old style",15))
+        cmb_gender.place(x=500,y=150,width=180)
+        cmb_gender.current(0)
+        txt_contact=Entry(self.root,textvariable=self.var_contact,font=("goudly old style",15),bg="light yellow").place(x=850,y=150,width=180)
+
+
+        #row2
+
+        lbl_name=Label(self.root,text="Name",font=("goudly old style",15),bg="white").place(x=50,y=190)
+        lbl_dob=Label(self.root,text="D.O.B",font=("goudly old style",15),bg="white").place(x=350,y=190)
+        lbl_doj=Label(self.root,text="D.O.J",font=("goudly old style",15),bg="white").place(x=750,y=190)
+
+        txt_name=Entry(self.root,textvariable=self.var_name,font=("goudly old style",15),bg="light yellow").place(x=150,y=190,width=180)
+        txt_dob=Entry(self.root,textvariable=self.var_dob,font=("goudly old style",15),bg="light yellow").place(x=500,y=190,width=180)
+        txt_doj=Entry(self.root,textvariable=self.var_doj,font=("goudly old style",15),bg="light yellow").place(x=850,y=190,width=180)
+
+
+        #row3
+
+        lbl_email=Label(self.root,text="Email",font=("goudly old style",15),bg="white").place(x=50,y=230)
+        lbl_password=Label(self.root,text="Password",font=("goudly old style",15),bg="white").place(x=350,y=230)
+        lbl_utype=Label(self.root,text="User Type",font=("goudly old style",15),bg="white").place(x=750,y=230)
+
+        txt_email=Entry(self.root,textvariable=self.var_email,font=("goudly old style",15),bg="light yellow").place(x=150,y=230,width=180)
+        txt_password=Entry(self.root,textvariable=self.var_pass,font=("goudly old style",15),bg="light yellow").place(x=500,y=230,width=180)
+        #txt_usertype=Entry(self.root,textvariable=self.var_utype,font=("goudly old style",15),bg="light yellow").place(x=850,y=230,width=180)
+        cmb_utype=ttk.Combobox(self.root,textvariable=self.var_utype,values=("Select","Admin","Employee"),state='readonly',justify=CENTER,font=("goudy old style",15))
+        cmb_utype.place(x=850,y=230,width=180)
+        cmb_utype.current(0)
+
+        #row4
+
+        lbl_address=Label(self.root,text="Address",font=("goudly old style",15),bg="white").place(x=50,y=270)
+        lbl_salary=Label(self.root,text="Salary",font=("goudly old style",15),bg="white").place(x=500,y=270)
+
+        self.txt_address=Text(self.root,font=("goudly old style",15),bg="light yellow")
+        self.txt_address.place(x=150,y=270,width=300,height=60)
+        txt_salary=Entry(self.root,textvariable=self.var_salary,font=("goudly old style",15),bg="light yellow").place(x=600,y=270,width=180)
+
+
+
+        #button
+        btn_add=Button(self.root,text="Save",font=("goudy old style",15),bg="#2196f3",fg="white",cursor="hand2").place(x=500,y=305,width=110,height=28)
+        btn_update=Button(self.root,text="Update",font=("goudy old style",15),bg="#4caf50",fg="white",cursor="hand2").place(x=620,y=305,width=110,height=28)
+        btn_delete=Button(self.root,text="Delete",font=("goudy old style",15),bg="#f44336",fg="white",cursor="hand2").place(x=740,y=305,width=110,height=28)
+        btn_clear=Button(self.root,text="Clear",font=("goudy old style",15),bg="#607d8b",fg="white",cursor="hand2").place(x=860,y=305,width=110,height=28)
+
+
+        
+
+
+
+
 
 if __name__ == "__main__":
     root=Tk()
